81706c1eb4d892f08174d48c37da3228eb2ebe37
Kumail Ali <m.kumailalirajpoot@gmail.com>
2025-03-12 20:19:21 +0500

    Banner is updated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import Header from "./components/Header";
 import HomeSlide from "./components/HomeSlide";
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 function App() {
+  console.log(URL);
+  if(URL === "/home") {
+    console.log("hello world this is a home slide")
+  }
   return (
     <div className="App w-[100%] relative flex justify-center flex-col">
       <Header></Header>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomeSlide/>}/>
+          <Route path="/Kid" element={<HomeSlide/>}/>
+          <Route path="/Men" element={<HomeSlide/>}/>
+          <Route path="/Lady" element={<HomeSlide/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { PiShoppingBagOpenBold } from "react-icons/pi";
 import {useEffect} from "react";
 const Nav = () => {
   useEffect(()=>{
@@ -12,12 +13,12 @@ const Nav = () => {
   },[])
   return (
     <nav data-aos="fade-down" className='flex justify-around items-center relative bg-slate-200'>
-        <h3 className='uppercase text-[2.5vmax] font-[500] text-gray-950'>sockseeker</h3>
+        <h3 className='uppercase flex text-[2.5vmax] items-center font-[500] text-gray-950'><PiShoppingBagOpenBold className='text-[3vmax]' />sockseeker</h3>
         <thead>
             <tr className='flex gap-[6vw] text-[1.3vmax] text-slate-950'>
                 <th className='nav-links'>Home</th>
                 <th className='nav-links'>Kids Socks</th>
-                <th className='nav-links'>Mens Socks</th>
+                <th className='nav-links'>Men Socks</th>
                 <th className='nav-links'>Lady Socks</th>
             </tr>
         </thead>
